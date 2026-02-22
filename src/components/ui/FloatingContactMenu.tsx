083da5a4dd5af6b2e857c46ca69f1d378c169db3
Thariqajees M N <117379444+Thariqs2143@users.noreply.github.com>
2026-02-22 15:27:58 +0530

    Update FloatingContactMenu.tsx

--- src/components/ui/FloatingContactMenu.tsx
+++ src/components/ui/FloatingContactMenu.tsx
@@ -131,7 +131,7 @@ export function FloatingContactMenu() {
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 16 16"
-              className="w-[40px] h-[40px]"
+              className="w-[52px] h-[52px]"
               aria-hidden="true"
             >
               <path
